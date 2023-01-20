e1e454b9a41606ba364dd9e214bcc4278cdfc14b
vivekkushal <vivekjss1@gmail.com>
2023-01-20 23:42:55 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -27,4 +27,22 @@ fn main() {
     let spaces = spaces.len();
     
     println!("The value of spaces is {spaces}");
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("{sum}, {difference}, {product}, {quotient}, {truncated}, {remainder}");
 }

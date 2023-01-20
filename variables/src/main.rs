833bf28b8e9d52cb016efc3b77de52649eeb954c
vivekkushal <vivekjss1@gmail.com>
2023-01-20 23:51:37 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -63,4 +63,14 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("The value of x is: {x}, y is: {y}, z is: {z}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("{five_hundred}, {six_point_four}, {one}");
 }

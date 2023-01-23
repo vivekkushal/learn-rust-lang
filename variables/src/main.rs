f81dd44ee4d50d0d4c8e5f170ee23ad2ca1946b0
vivekkushal <vivekjss1@gmail.com>
2023-01-23 23:57:40 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    // let mut x = 5;
-    // println!("The value of x is: {x}");
-    // x = 6;
-    // println!("The value of x is: {x}");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 
-    // let y = x + 10;
-    // println!("The value of y is: {y}");
+    let y = x + 10;
+    println!("The value of y is: {y}");
 
-    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    // println!("The value of THREE_HOURS_IN_SECOND is: {THREE_HOURS_IN_SECONDS}");
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECOND is: {THREE_HOURS_IN_SECONDS}");
 
     // Shadowing example
     let x = 5;

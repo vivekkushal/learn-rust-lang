77278829ee9131a6884b92a766261c6143577c77
vivekkushal <vivekjss1@gmail.com>
2023-02-09 23:58:25 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -66,11 +66,11 @@ fn main() {
 
     // let x: (i32, f64, u8) = (500, 6.4, 1);
 
-    // let five_hundred = x.0;
+    let five_hundred = x.0;
 
-    // let six_point_four = x.1;
+    let six_point_four = x.1;
 
-    // let one = x.2;
+    let one = x.2;
 
-    // println!("{five_hundred}, {six_point_four}, {one}");
+    println!("{five_hundred}, {six_point_four}, {one}");
 }

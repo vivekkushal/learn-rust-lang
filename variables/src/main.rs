c829378ec088d9d74d2dcfb9c0e180b10dc37c79
vivekkushal <vivekjss1@gmail.com>
2023-02-13 23:47:31 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,17 +10,17 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECOND is: {THREE_HOURS_IN_SECONDS}");
 
-    // Shadowing example
-    let x = 5;
+    // // Shadowing example
+    // let x = 5;
 
-    let x = x + 1;
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
 
-    println!("The value of x is: {x}");
+    // println!("The value of x is: {x}");
 
     // // Shadowing another example
     // let spaces = "   ";

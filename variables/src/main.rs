f4524a08c40df9f2dd3e9a7b746c810fb2b64dfc
vivekkushal <vivekjss1@gmail.com>
2023-02-18 23:58:29 +0530

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -20,13 +20,13 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
 
-    // println!("The value of x is: {x}");
+    println!("The value of x is: {x}");
 
-    // // Shadowing another example
-    // let spaces = "   ";
-    // let spaces = spaces.len();
+    // Shadowing another example
+    let spaces = "   ";
+    let spaces = spaces.len();
     
-    // println!("The value of spaces is {spaces}");
+    println!("The value of spaces is {spaces}");
 
     // // addition
     // let sum = 5 + 10;
